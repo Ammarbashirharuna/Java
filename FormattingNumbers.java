204a772c6ed0e677bc5ed8b2ce04b32c6c8e78f7
ammar <ammarbashirharuna2020@gmail.com>
2024-12-17 03:42:18 +0100

    formatting numbers

---
+++ FormattingNumbers.java
@@ -0,0 +1,8 @@
+import java.text.NumberFormat;
+public class FormattingNumbers {
+    public static void main(String[] args) {
+        String result = NumberFormat.getPercentInstance().format(0.1 );
+        System.out.println("parcent: " + result);
+
+    }
+}
