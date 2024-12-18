c5d5a9100641476f6e486b2921ee1092383f2d27
ammar <ammarbashirharuna2020@gmail.com>
2024-12-18 19:21:12 +0100

    fizzbuzz program

---
+++ programs/FizzBuzz.java
@@ -0,0 +1,22 @@
+package programs;
+import java.util.Scanner;
+
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        System.out.print("Enter a Number");
+        Scanner number = new Scanner(System.in);
+
+        int enteredNumber = number.nextInt();
+        if (enteredNumber % 5 == 0 && enteredNumber % 3 == 0) {
+            System.out.println("FizzBuzz"); 
+        } else if (enteredNumber % 5 == 0) {
+            System.out.println("Fizz");
+        } else if (enteredNumber % 3 == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(enteredNumber);
+        }
+        number.close();
+    }
+}
