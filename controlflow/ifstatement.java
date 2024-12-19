8e9b8c64e1a4c5a3a42b665b6b75602e2f4e2a41
ammar <ammarbashirharuna2020@gmail.com>
2024-12-19 22:59:41 +0100

    if statement

---
+++ controlflow/ifstatement.java
@@ -0,0 +1,11 @@
+package controlflow;
+
+public class ifstatement {
+    public static void main(String[] args) {
+        int temprature = 32;
+        if (temprature > 20) {
+            System.out.println("its a hot day");
+            System.out.println("Drink water ");
+        }
+    }
+}
