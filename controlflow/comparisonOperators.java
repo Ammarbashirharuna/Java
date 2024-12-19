5cc71aff8148192d42ec11ff2c7f8bbce8685997
ammar <ammarbashirharuna2020@gmail.com>
2024-12-19 19:16:51 +0100

    a code

--- controlflow/comparisonOperators.java
+++ controlflow/comparisonOperators.java
@@ -4,7 +4,8 @@ public class comparisonOperators {
     public static void main(String[] args) {
         boolean isAdult = 15 >= 16;
         System.out.println(isAdult);
-        boolean  = 13 <= 18;
+        boolean isYoung = 13 <= 18;
+        System.out.println(isYoung);
 
     }
 }
