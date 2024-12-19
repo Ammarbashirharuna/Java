4f712067d91af31fe97e0f496387ff28257dd341
ammar <ammarbashirharuna2020@gmail.com>
2024-12-19 18:13:01 +0100

    increment

---
+++ Types/IncrementDecrement.java
@@ -0,0 +1,9 @@
+package Types;
+
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        int number = 30;
+        int increase = number+ 1;
+        System.out.println(increase);
+    }
+}
