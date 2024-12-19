c03a4c5849ce934b063ac1d0dc9684832806275c
ammar <ammarbashirharuna2020@gmail.com>
2024-12-19 23:07:44 +0100

    refactoring the code

--- controlflow/ifstatement.java
+++ controlflow/ifstatement.java
@@ -6,11 +6,11 @@ public class ifstatement {
         if (temprature > 20) {
             System.out.println("its a hot day");
             System.out.println("Drink water ");
-        } else if (temprature > 20 && temprature < 32) {
+        } 
+        else if (temprature > 20 && temprature < 32)
             System.out.println("beautiful day");
-            
-        } else {
+         else
             System.out.println("Cold day");
-        }
+        
     }
 }
