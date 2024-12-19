c9fd6a67ee857de5a1e91a6053247a4b528cbe93
ammar <ammarbashirharuna2020@gmail.com>
2024-12-20 00:26:45 +0100

    using parenthesis

--- controlflow/forLoops.java
+++ controlflow/forLoops.java
@@ -4,6 +4,11 @@ public class forLoops {
     public static void main(String[] args) {
         // when we only have single statement to execute no need for braces
         for (int i = 0; i < 10; i++)
-            System.out.println("Hello worl");
+            System.out.println("Hello world" + i);
+            // using parenthesis
+        for (int i = 0; i < 5; i++){
+            System.out.println("i really love nabila" + i);
+            System.out.println("and i hope she sleep well");
+        }
     }
 }
