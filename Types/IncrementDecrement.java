9b6630b8aaa1e3071c2ddf1432243006699a1869
ammar <ammarbashirharuna2020@gmail.com>
2024-12-19 19:03:14 +0100

    decrement algo

--- Types/IncrementDecrement.java
+++ Types/IncrementDecrement.java
@@ -3,7 +3,10 @@ package Types;
 public class IncrementDecrement {
     public static void main(String[] args) {
         int number = 30;
-        int increase = number+ 1;
-        System.out.println(increase);
+        System.out.println(number++);
+        System.out.println(number);
+        int numberdec = 40;
+        System.out.println(numberdec--);
+        System.out.println(numberdec);
     }
 }
