8e8684d82fba71d8b425a05b771c749949c845c4
ammar <ammarbashirharuna2020@gmail.com>
2024-12-19 22:25:12 +0100

    logical && operator

---
+++ controlflow/logivcalOperators.java
@@ -0,0 +1,11 @@
+package controlflow;
+
+public class logivcalOperators {
+    public static void main(String[] args) {
+        int temprature = 22;
+        boolean isWarm = temprature > 20 && temprature < 30;
+        System.out.println(isWarm);
+        
+
+    }
+}
