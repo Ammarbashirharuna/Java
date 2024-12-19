c3732501f65295fbe5cff26859e263c459a75d77
ammar <ammarbashirharuna2020@gmail.com>
2024-12-19 23:55:49 +0100

    switch statement

---
+++ controlflow/switchStatement.java
@@ -0,0 +1,18 @@
+package controlflow;
+
+public class switchStatement {
+    public static void main(String[] args) {
+        String role = "admin";
+        switch (role) {
+            case "admin":
+                System.out.println("Welcome sir hope you are doing well");
+                break;
+            case "modirator":
+                System.out.println("Welcome modirator");
+                break;
+        
+            default:
+                
+        }
+    }
+}
