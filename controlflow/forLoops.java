bc00da7013e34de3cd70d815167f35560a73385f
ammar <ammarbashirharuna2020@gmail.com>
2024-12-20 00:20:59 +0100

    initalizing the loop

--- controlflow/forLoops.java
+++ controlflow/forLoops.java
@@ -2,6 +2,8 @@ package controlflow;
 
 public class forLoops {
     public static void main(String[] args) {
-        
+        // when we only have single statement to execute no need for braces
+        for (int i = 0; i < 10; i++)
+            System.out.println("Hello worl");
     }
 }
