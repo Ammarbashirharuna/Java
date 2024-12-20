6fbf9b1bba891aa00a6e0498cf8dd2446bb02b61
ammar <ammarbashirharuna2020@gmail.com>
2024-12-20 11:40:18 +0100

    cleancoding

---
+++ cleancoding/creatingMethod.java
@@ -0,0 +1,12 @@
+ package cleancoding;
+
+public class creatingMethod {
+    public static void main(String[] args) {
+
+    }
+
+    public static void greatUSer(String name) {
+        
+    }
+    
+}
