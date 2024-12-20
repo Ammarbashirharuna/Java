ce8365f3e0efb19e744571fb45a791e14083f932
ammar <ammarbashirharuna2020@gmail.com>
2024-12-20 11:43:35 +0100

    creating method

--- cleancoding/creatingMethod.java
+++ cleancoding/creatingMethod.java
@@ -2,11 +2,11 @@
 
 public class creatingMethod {
     public static void main(String[] args) {
-
+        greatUSer("Ammar");
     }
 
     public static void greatUSer(String name) {
-        
+        System.out.println("Hello" + name);
     }
-    
+
 }
