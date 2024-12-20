9e2eae4292ca17e67498531c43a57d1abd920768
ammar <ammarbashirharuna2020@gmail.com>
2024-12-20 11:45:07 +0100

    recalling method

--- cleancoding/creatingMethod.java
+++ cleancoding/creatingMethod.java
@@ -3,10 +3,11 @@
 public class creatingMethod {
     public static void main(String[] args) {
         greatUSer("Ammar");
+        greatUSer("muhammad");
     }
 
     public static void greatUSer(String name) {
-        System.out.println("Hello" + name);
+        System.out.println("Hello " + name);
     }
 
 }
