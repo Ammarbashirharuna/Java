ebe1fa2df73075abe915246a98df0fe2f7955e13
ammar <ammarbashirharuna2020@gmail.com>
2024-12-20 01:04:33 +0100

    trying

--- controlflow/whileLoops.java
+++ controlflow/whileLoops.java
@@ -9,16 +9,25 @@ public class whileLoops {
             System.out.print("Enter Value: ");
             inpute = input.next();
             System.out.println(inpute);
-       }
-       input.close();
-// trying to create  another scanner and while loops
-       Scanner collect = new Scanner(System.in);
-       int num1 = 3;
-       while (num1 != 3) {
-           System.out.print("Enter a number to check: ");
-           num1 = collect.nextInt();
-           System.out.println(num1);
-       }
-       collect.close();
+        }
+        input.close();
+        // trying to create  another scanner and while loops
+        Scanner collect = new Scanner(System.in);
+        int num1 = 3;
+        while (num1 != 3) {
+            System.out.print("Enter a number to check: ");
+            num1 = collect.nextInt();
+            System.out.println(num1);
+        }
+        collect.close();
+    }
+
+    Scanner accept = new Scanner(System.in);
+    String admin = "";
+    while(!admin.equals("Ammar"))
+    {
+        System.out.print("Enter admin name: ");
+        
+
     }
 }
