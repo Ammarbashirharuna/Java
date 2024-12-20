994456b2e355bf9283aa0a90409ff1e42c74a2d1
ammar <ammarbashirharuna2020@gmail.com>
2024-12-20 02:00:53 +0100

    implementing for each for simplicity

--- controlflow/forEachLoop.java
+++ controlflow/forEachLoop.java
@@ -6,6 +6,10 @@ public class forEachLoop {
 
     // using for loop to iterate over the item 
     for (int i = 0; i < fruits.length; i++)
-       System.out.println(fruits[i]);
+        System.out.println(fruits[i]);
+    //    using for each loop
+    for (String fruit : fruits)
+        System.out.println(fruit);
+    
     }
 }
