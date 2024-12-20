967935544a6eeeee2b3f710bd01927670bd1766f
ammar <ammarbashirharuna2020@gmail.com>
2024-12-20 01:57:32 +0100

    addin the logic

--- controlflow/forEachLoop.java
+++ controlflow/forEachLoop.java
@@ -2,6 +2,10 @@ package controlflow;
 
 public class forEachLoop {
     public static void main(String[] args) {
-        
+        String[] fruits =  {"mango", "apple", "Banana", "Guava"};
+
+    // using for loop to iterate over the item 
+    for (int i = 0; i < fruits.length; i++)
+       System.out.println(fruits[i]);
     }
 }
