1a383592dc9bcebf5c7629be927fcaf17943ebb4
ammar <ammarbashirharuna2020@gmail.com>
2024-12-20 11:57:52 +0100

    creating metho that retun string value

--- cleancoding/creatingMethod.java
+++ cleancoding/creatingMethod.java
@@ -1,13 +1,20 @@
- package cleancoding;
+package cleancoding;
 
 public class creatingMethod {
     public static void main(String[] args) {
         greatUSer("Ammar", "Bashir");
-        
+        String messege = greatAdmin("Ammar");
+        System.out.println(messege);
+
     }
 
     public static void greatUSer(String firstName, String lastName) {
-        System.out.println("Hello " + firstName  + " " + lastName);
+        System.out.println("Hello " + firstName + " " + lastName);
+    }
+
+    // methods that return a string value
+    public static String greatAdmin(String admin) {
+        return "Hello" + admin;
     }
 
 }
