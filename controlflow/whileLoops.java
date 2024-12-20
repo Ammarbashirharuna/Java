401b377afc077f7f3db52de8df5477f227bfc03d
ammar <ammarbashirharuna2020@gmail.com>
2024-12-20 01:19:06 +0100

    making the code more readable

--- controlflow/whileLoops.java
+++ controlflow/whileLoops.java
@@ -3,16 +3,18 @@ import java.util.Scanner;
 
 public class whileLoops {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
+        // first loop
         String inpute = "";
         while (!inpute.equals("quite")) {
             System.out.print("Enter Value: ");
             inpute = input.next();
             System.out.println(inpute);
         }
-        input.close();
+       
         // trying to create  another scanner and while loops
-        Scanner collect = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         int num1 = 3;
         while (num1 != 3) {
             System.out.print("Enter a number to check: ");
