c303fcd2c2b7c3b35e220586ded9e5656868db93
ammar <ammarbashirharuna2020@gmail.com>
2024-12-25 18:16:41 +0100

    adding some code .

--- OOP/src/Main.java
+++ OOP/src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-    int baseSalary = 50_000;
-    int extraHours = 10;
-    int hourlyRate = 20;
-//    calling the employee method
-        int wage = calculateWage(baseSalary, extraHours, hourlyRate);
+        var employee = new Employee();
+        employee.hourlyRate = 50_000;
+        employee.baseSalary = 20;
+        int wage = employee.calculateWage(10);
         System.out.println(wage);
 
 
+
+
     }
 
-    public  static int calculateWage(int baseSalary, int extraHours, int hourlyRate){
-        return baseSalary + (extraHours * hourlyRate);
-    }
+
+
 
 }
