3f8cb3b78e0467f4324b6d5084e8d77ae7a3d909
ammar <ammarbashirharuna2020@gmail.com>
2024-12-25 18:10:24 +0100

    encapsolation

--- OOP/Main.java
+++
@@ -1,9 +0,0 @@
-package OOP;
-
-public class Main {
-    public static void main(String[] args) {
-        var TextBox1 = new TextBox();
-        TextBox1.SetText("Hello guys this is box 1");
-        System.out.println(TextBox1);
-    }
-}
